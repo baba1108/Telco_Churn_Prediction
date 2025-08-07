02b8eeef59fc48e5790453360b65fe9006cf19e7
baba1108 <babasy1122@icloud.com>
2025-08-07 13:47:33 +0000

    Update telco_churn.py

--- telco_churn.py
+++ telco_churn.py
@@ -30,7 +30,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # SMOTE to handle imbalance
 sm = SMOTE(random_state=42)
-X_train_smote, y_train_smote = sm.fit_sample(X_train, y_train)
+X_train_smote, y_train_smote = sm.fit_resample(X_train, y_train)
 
 # Train or load model
 MODEL_PATH = "rf_model.pkl"
